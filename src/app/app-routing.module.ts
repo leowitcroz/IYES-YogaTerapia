95260936ff5f601ea6098333e92f0233f6f396d5
Leonardo <leowitcroz@gmail.com>
2022-08-24 11:08:22 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GeralComponent } from './page/geral/geral.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',component:GeralComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { GeralComponent } from './page/geral/geral.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    GeralComponent
   ],
   imports: [
     BrowserModule,
